a9be7d2ceeb95ba862de4bf115d905d452960961
Berhane Berhane <berhane896@gmail.com>
2026-02-03 06:53:02 +0000

    created bookmark model

---
+++ app/models/bookmark.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: bookmarks
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  listing_id :integer
+#  user_id    :integer
+#
+class Bookmark < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_03_063338) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_03_065223) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "listing_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "listings", force: :cascade do |t|
     t.integer "user_id"
