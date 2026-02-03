4850c2d5450560cb75f678edf652880deda0a3f2
Berhane Berhane <berhane896@gmail.com>
2026-02-03 20:53:43 +0000

    adjusted routes and specified actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,9 @@
 # config/routes.rb
 Rails.application.routes.draw do
   devise_for :users
-  
-  root 'listings#index'
-  
-  resources :listings do
-    member do
-      post 'bookmark'
-      delete 'unbookmark'
-    end
-    collection do
-      get 'search'
-    end
-  end
-  
-  resources :bookmarks, only: [:index]
-  
-  # JSON API endpoints
-  namespace :api do
-    namespace :v1 do
-      resources :listings, only: [:index, :show]
-    end
-  end
+
+  root to: "listings#index"
+
+  resources :listings, only: [:index, :show]
+resources :bookmarks, only: [:create, :destroy, :index]
 end
