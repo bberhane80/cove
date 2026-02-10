523be47289e2894c189aa1f2fba9b8e2ed993cf1
Berhane Berhane <berhane896@gmail.com>
2026-02-10 00:55:33 +0000

    created more styling partials, added helpers methods, and added scheduling options for ai mailer

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,19 @@
-every :sunday, at: '9am' do
+# Daily at 9 AM
+every :day, at: '9:00am' do
+  rake "recommendations:send_daily"
+end
+
+# Weekly on Sunday at 9 AM
+every :sunday, at: '9:00am' do
   rake "recommendations:send_weekly"
+end
+
+# Bi-weekly (every 2 weeks) on Sunday at 9 AM
+every 2.weeks, at: '9:00am' do
+  rake "recommendations:send_biweekly"
+end
+
+# Monthly on the 1st at 9 AM
+every '0 9 1 * *' do
+  rake "recommendations:send_monthly"
 end
