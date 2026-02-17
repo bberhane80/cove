13e07941f8cdbcd8a56d331e9a1c079b8afb4bcf
Berhane Berhane <berhane896@gmail.com>
2026-02-17 02:43:33 +0000

    added footer and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
-# config/routes.rb
 Rails.application.routes.draw do
   devise_for :users
-
-  root to: "listings#index"
-
-resources :users, only: [:show, :edit, :update]
-resources :listings, only: [:index, :show]
-resources :bookmarks, only: [:create, :destroy, :index]
+  
+  root 'listings#index'
+  
+  resources :listings, only: [:index, :show]
+  resources :bookmarks, only: [:create, :destroy, :index]
+  resources :users, only: [:show, :edit, :update]
+  
+  # About page
+  get 'about', to: 'pages#about'
+  
+  # Health check
+  get '/health', to: 'health#index'
 end
